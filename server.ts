b499f2bed525a12e8c8eef7c0779fa5b8e8e7c92
Ariel Zamir <arielzamir100@gmail.com>
2023-11-13 15:08:48 +0200

    Adjusted the code

--- server.ts
+++ server.ts
@@ -1,22 +1,46 @@
 import express from "express";
-import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
 
-async function Connect() {
+const uri = "mongodb+srv://ariel:1234@cluster0.v3rhybd.mongodb.net/";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const client = new MongoClient(uri);
+
+async function run() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://ariel:1234@cluster0.v3rhybd.mongodb.net/"
-    );
-    console.log("Connected to MongoDb");
-  } catch (error) {
-    console.log("Error connecting to MongoDB", error);
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    // Access the collection
+    const collection = client.db("my-store").collection("products");
+
+    // Query for all documents in the collection
+    const cursor = collection.find();
+
+    // Convert the cursor to an array of documents
+    const documents = await cursor.toArray();
+
+    console.log("Retrieved documents:", documents);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
 
-Connect();
+// Define an asynchronous function to start the server after connecting to MongoDB
+async function startServer() {
+  try {
+    await run();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Call the function to start the server
+startServer();
