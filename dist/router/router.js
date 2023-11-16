50de907b7360cf01af16c65fd62040c1f620e5a7
Ariel Zamir <arielzamir100@gmail.com>
2023-11-16 10:08:34 +0200

    Change the structure of the project

---
+++ dist/router/router.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const controller_1 = require("../controller/controller");
+const router = express_1.default.Router();
+router.post("/login", controller_1.loginController);
+router.post("/register", controller_1.registerController);
+router.post("/add/:id", controller_1.addProductToCartController);
+router.post('/ratingP/:id', controller_1.clickUpdateProductController);
+router.post('/ratingC/:id', controller_1.clickUpdateCategoryController);
+router.get("/products", controller_1.getAllProductsController);
+router.get("/:category", controller_1.getProductByCategoryController);
+router.get("/all/categories", controller_1.getAllCategoryController);
+exports.default = router;
