fb0a90a9e37a1f7b06a42f8c62552a8fa07d9593
Ariel Zamir <arielzamir100@gmail.com>
2023-11-16 17:16:48 +0200

    Added a file for mongodb conncetion

--- src/dal/dal.ts
+++ src/dal/dal.ts
@@ -1,31 +1,17 @@
-import { MongoClient } from "mongodb";
-import { UserData, Product } from "../interfaces";
+import { UserData, Product } from "../interfaces/interfaces";
+import { client } from "../mongodbConnection/connectToDatabase";
 
-const url = "mongodb+srv://ariel:1234@cluster0.v3rhybd.mongodb.net/";
-const client = new MongoClient(url);
 const dbName = "my-store";
-
-async function run() {
-  try {
-    await client.connect();
-    console.log("Successfully connected to Atlas");
-  } catch (err) {
-    console.log(err);
-  }
-}
 
 const getAllProducts = async () => {
   try {
-    await client.connect();
     const db = client.db(dbName);
     const col = db.collection("products");
-    const products =await col.find({}).sort({ rating: -1 }).toArray();
+    const products = await col.find({}).sort({ rating: -1 }).toArray();
     console.log(products);
     return products;
   } catch (err) {
     console.log(err);
-  } finally {
-    await client.close();
   }
 };
 
@@ -35,14 +21,12 @@ const getProductsByCategory = async (category: string) => {
     const db = client.db(dbName);
     const col = db.collection("products");
     const products = await col
-      .find({ 'category.name': `${category}` })
+      .find({ "category.name": `${category}` })
       .sort({ "commonAttributes.price": -1 })
       .toArray();
     return products;
   } catch (err) {
     console.log(err);
-  } finally {
-    await client.close();
   }
 };
 
@@ -55,8 +39,6 @@ const getAllCategories = async () => {
     return category;
   } catch (err) {
     console.log(err);
-  } finally {
-    await client.close();
   }
 };
 
@@ -118,36 +100,31 @@ const register = async (data: UserData) => {
   }
 };
 
-const clickUpdateProduct = async (id:number)=>{
+const clickUpdateProduct = async (id: number) => {
   try {
     await client.connect();
     const db = client.db(dbName);
     const col = db.collection("products");
     await col.updateOne({ id }, { $inc: { rating: 1 } });
-    return 1
+    return 1;
   } catch (err) {
     console.log(err);
-  } finally {
-    await client.close();
   }
-}
+};
 
-const clickUpdateCategory = async (id:number)=>{
+const clickUpdateCategory = async (id: number) => {
   try {
     await client.connect();
     const db = client.db(dbName);
     const col = db.collection("category");
     await col.updateOne({ id }, { $inc: { rating: 1 } });
-    return 1
+    return 1;
   } catch (err) {
     console.log(err);
-  } finally {
-    await client.close();
   }
-}
+};
 
 export {
-  run,
   getAllProducts,
   getProductsByCategory,
   getAllCategories,
--- src/server.ts
+++ src/server.ts
@@ -3,29 +3,28 @@ import router from "./router/router";
 import * as connect from "./dal/dal"
 import morgan from "morgan";
 import cors from "cors";
+import { Express } from "express";
+import { connectToDatabase } from "./mongodbConnection/connectToDatabase";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
-
 app.use(cors());
 app.use(express.json());
-
 app.use("/api", router);
 
-async function startServer() {
+async function startServer(app: Express) {
   try {
-    await connect.run();
+    await connectToDatabase();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting the server:", error);
-    process.exit(1); // Exit the process with a non-zero code to indicate an error
   }
 }
 
 // Call the function to start the server
-startServer();
+startServer(app);
 
